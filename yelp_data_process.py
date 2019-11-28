ec069790f57de51b075f8a2a42db738f7eedb974
LI LI <lee_li1991@qq.com>
2019-11-28 17:31:12 +0800

    convert business id from string to int

--- yelp_data_process.py
+++ yelp_data_process.py
@@ -5,10 +5,11 @@ import sys
 spark = SparkSession.builder.appName('data_process').getOrCreate()
 
 def main():
-	#step 1 convert string busines id to int id
+	#step 1 convert string business id to int id
 	schema_bid = StructType([
-        StructField('business_id', StringType(), False)       
+        StructField('business_id', StringType(), False)
     ])
+    
     yelp_business = spark.read.json("/business.json", schema_bid)
     yelp_business.createOrReplaceTempView('yelp_business')
     businiess_id = spark.sql(""" SELECT DISTINCT business_id FROM yelp_business""")
@@ -16,6 +17,6 @@ def main():
     # businiess_id.write.save("/Bid", format='csv', mode='overwrite')
     businiess_id.show()
     spark.stop()
-    
+
 if __name__ == "__main__":
     main()
